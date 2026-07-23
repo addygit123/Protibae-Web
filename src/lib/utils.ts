371112dd9d846997395f59b99ba4f227d3e3ccc7
Adarsh Vishwakarma <minecraftaddy123@gmail.com>
2026-07-23 23:56:40 +0530

    fixed forget password link

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -6,17 +6,17 @@ export function cn(...inputs: ClassValue[]) {
 }
 
 export function getBaseUrl() {
-  if (process.env.NEXT_PUBLIC_SITE_URL && !process.env.NEXT_PUBLIC_SITE_URL.includes('localhost')) {
-    return process.env.NEXT_PUBLIC_SITE_URL;
-  }
-  if (process.env.NEXTAUTH_URL && !process.env.NEXTAUTH_URL.includes('localhost')) {
-    return process.env.NEXTAUTH_URL;
-  }
   if (process.env.VERCEL_PROJECT_PRODUCTION_URL) {
     return `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`;
   }
   if (process.env.VERCEL_URL) {
     return `https://${process.env.VERCEL_URL}`;
+  }
+  if (process.env.NEXT_PUBLIC_SITE_URL && !process.env.NEXT_PUBLIC_SITE_URL.includes('localhost')) {
+    return process.env.NEXT_PUBLIC_SITE_URL;
+  }
+  if (process.env.NEXTAUTH_URL && !process.env.NEXTAUTH_URL.includes('localhost')) {
+    return process.env.NEXTAUTH_URL;
   }
   return process.env.NEXT_PUBLIC_SITE_URL || 'http://localhost:3000';
 }
